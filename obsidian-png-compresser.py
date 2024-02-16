88892a75d46e53cf68981acdaaa9e259cd912550
andreas <andreas@pedersenmail.com>
2024-02-16 13:45:47 +0100

    Finish png-compresser Script
    Look into deprecated Palette.

--- obsidian-png-compresser.py
+++ obsidian-png-compresser.py
@@ -1,16 +1,16 @@
 from PIL import Image
 import os
 
-# img = Image.open('testimg.png')
-# img = img.convert("P", palette=Image.ADAPTIVE, colors=256)
-# img.save("comp.png", optimize=True)
-
-obsidian_dir = r"C:/Obsidian/MainVault"
+obsidian_dir = r"C:/Obsidian/MainVault/University/6.semester"
 
 imgs = []
 for root, dirs, files in os.walk(obsidian_dir):
     for file in files:
         if ".obsidian" not in root:
             if file.endswith(".png"):
-                imgs.append(os.path.join(root, file))
+                print("Compressing {}".format(os.path.join(root, file)))
+                img = Image.open(os.path.join(root, file))
+                img = img.convert("P", palette=Image.ADAPTIVE, colors=256)
+                img.save(os.path.join(root, file), optimize=True)
+                
                 
