cc6b0ccce1f956f3b1645e8804f1a6c96fd33485
andreasp <andreas@pedersenmail.com>
2024-05-06 11:39:45 +0200

    add cmrr mode

--- gain.py
+++ gain.py
@@ -5,10 +5,11 @@ parser = argparse.ArgumentParser(description="Gain Converter")
 parser.add_argument("-r", "--ratio", action="store_true", help="voltage/power gain as ratio (default of no option)")
 parser.add_argument("-d", "--db", action="store_true", help="voltage/power gain in dB")
 parser.add_argument("-p", "--power", action="store_true", help="power gain")
+parser.add_argument("-c", "--cmrr", action="store_true", help="common mode rejection ratio")
 parser.add_argument("gain")
 args = parser.parse_args()
 
-if not any([args.ratio, args.db]):
+if not any([args.ratio, args.db, args.cmrr]):
     args.ratio = True
 
 # Voltage gain = 20 * log10(Vout/Vin)
@@ -26,3 +27,8 @@ elif args.db:
     gain = float(args.gain)
     print(f"Gain in dB: {gain}")
     print(f"Gain ratio: {np.round(10 ** (gain / prod), 2):,}")
+    
+elif args.cmrr:
+    gain = float(args.gain)
+    print(f"CMRR: {gain}")
+    print(f"CM Gain: Adm / {np.round(10 ** (gain / 20),2)}")
